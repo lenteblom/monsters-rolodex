25d2497f11f8c0a5154c16b0ae8c5199ed345968
Heidi Kruger <heidik@urup.com>
2020-05-20 14:41:47 +0200

    Updates

---
+++ precache-manifest.b39cae289e0755b9df73aa0415b6bac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53456a7a0885786a23acd03f28835890",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b96d85f0c4e5edb85bf5",
+    "url": "/monsters-rolodex/static/css/main.c14592c0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96d85f0c4e5edb85bf5",
+    "url": "/monsters-rolodex/static/js/main.f6ed5bd8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
